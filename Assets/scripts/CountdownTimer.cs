898576f58a3ddfe736eecb5dcc8b9c3446d4db28
matthew930823 <matthew930823@gmail.com>
2025-08-04 21:28:56 +0800

    0804

--- Assets/scripts/CountdownTimer.cs
+++ Assets/scripts/CountdownTimer.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro; // 如果你使用 TextMeshPro，請加這行
+using System.Collections.Generic;
 
 public class CountdownTimer : MonoBehaviour
 {
@@ -55,10 +56,24 @@ public class CountdownTimer : MonoBehaviour
     {
         Debug.Log("時間到！");
         // 你可以在這裡觸發遊戲結束、顯示畫面等動作
-        Score.text = "Score : " + selectionBox.B.Count;
+        int score = selectionBox.B.Count * 10;
+        if (score >= 100)
+        {
+            gameController.voiceAudioPlayer.AudioPlay(7);
+        }
+        else if(score >= 60)
+        {
+            gameController.voiceAudioPlayer.AudioPlay(8);
+        }
+        else
+        {
+            gameController.voiceAudioPlayer.AudioPlay(9);
+        }
+        Score.text = "Score : " + score;
         ResultImage.sprite = GameImage.sprite;
-        gameController.voiceAudioPlayer.AudioPlay(10);
+        //gameController.voiceAudioPlayer.AudioPlay(10);
         ResultScreen.SetActive(true);
         GameScreen.SetActive(false);
+        selectionBox.B = new List<string>();
     }
 }
